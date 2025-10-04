77f111d480fa999423536366b632bb6a1d216544
anshjoshii <amar707026@gmail.com>
2025-10-04 22:49:56 +0530

    swapping elememts using xor

---
+++ xor.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    a = a ^ b;  // Step 1
+    b = a ^ b;  // Step 2
+    a = a ^ b;  // Step 3
+
+    printf("After swapping: a = %d, b = %d\n", a, b);
+    return 0;
+}
