3ce009128d8efc9d8ddfdd4196e592c55f88d73a
anshjoshii <amar707026@gmail.com>
2025-10-12 01:20:56 +0530

    printing original aray

--- linearsearch.c
+++ linearsearch.c
@@ -14,15 +14,23 @@ int main() {
     for(i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
-
+    // printing array
+    printf("Array is : ");
+    printf("[");
+    for(i=0;i<n;i++)
+    {
+      printf(" %d ",  arr[i]);
+    }
+    printf("]");
     // Taking element to search
+    printf("\n");
     printf("Enter the element to search: ");
     scanf("%d", &search);
 
     // Performing Linear Search
     for(i = 0; i < n; i++) {
         if(arr[i] == search) {
-            printf("Element found at position %d and its index nnumber is %d \n", i + 1,i);
+            printf("Element %d is found at position %d and its index nnumber is %d \n",search, i + 1,i);
             flag = 1;
             break;
         }
